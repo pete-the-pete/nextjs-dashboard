cf8e157c03f84fd312f23bd1ac8af9dcd66be1fd
pete-the-pete <pliljegr@gmail.com>
2025-04-15 17:38:43 -0700

    how do i run the vercel build locally?

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,8 +1,7 @@
-import CardWrapper from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
-import { CardsSkeleton, InvoiceSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { InvoiceSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 
 export default async function Page() {
@@ -12,9 +11,9 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Suspense fallback={<CardsSkeleton />}>
+                {/* <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper />
-                </Suspense>
+                </Suspense> */}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<RevenueChartSkeleton />}>
